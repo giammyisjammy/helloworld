0eb1ec518140cfffad97e475b34ac0d77fa95599
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2022-09-30 16:10:08 +0200

    ⚡️ Remove parsing nonsense
    Also cleaner and formatted code

--- src/main/scala/rps/Game.scala
+++ src/main/scala/rps/Game.scala
@@ -7,18 +7,16 @@ object Game {
 
   // TODO this should be an enum (or Scala equivalent of TS Enum)
   object Weapon {
-    val Rock: Int = 0 // 🪨
-    val Paper: Int = 1 // 📄
-    val Scissor: Int = 2 // ✂️
-    val Invalid: Int = -1 // Something bad happened
+    val Rock: String = "0" // 🪨
+    val Paper: String = "1" // 📄
+    val Scissor: String = "2" // ✂️
   }
 
   // TODO this should be an enum (or Scala equivalent of TS Enum)
   object Winner {
-    val Draw: Int = 0 // It's a draw
-    val User: Int = 1 // User wins
-    val Cpu: Int = 2 // Cpu wins
-    val Invalid: Int = -1 // Something bad happened
+    val Draw: String = "0" // It's a draw
+    val User: String = "1" // User wins
+    val Cpu: String = "2" // Cpu wins
   }
 
   def play(): Unit = {
@@ -28,63 +26,33 @@ object Game {
     ${Weapon.Scissor} - ✂️ Scissor
     """)
     val rawUserInput = readLine()
-    val userWeapon = parseWeapon(rawUserInput)
-    val cpuWeapon = parseWeapon(generateComputerMove())
+    val userWeapon = rawUserInput
+    val cpuWeapon = generateComputerMove()
 
-    if (userWeapon != Weapon.Invalid) {
-      println(s"Your opponent chose ${cpuWeapon}")
+    val result = checkWinner(userWeapon, cpuWeapon)
 
-      val result = checkWinner(userWeapon, cpuWeapon)
-      announceWinner(result)
-    } else {
-      println("Not a valid weapon, Spock!")
-    }
+    println(s"You chose: ${userWeapon} | your opponent chose: ${cpuWeapon}")
+    println(announceWinner(result))
   }
 
-  def announceWinner(result: Int): Unit = {
-    val winner =
-      if (result == Winner.Cpu) "Cpu"
-      else if (result == Winner.User) "User"
-      else "dafuq"
-    val emoji =
-      if (result == Winner.Cpu) "🤷"
-      else if (result == Winner.User) "🎉"
-      else "😱"
-
-    val message =
-      if (result == Winner.Draw) "It's a draw ✍️"
-      else s"${winner} Wins ${emoji}"
-    if (result != Winner.Invalid) println(message)
+  def announceWinner(result: String): String = {
+    if (result == Winner.Draw) "It's a draw ✍️"
+    else if (result == Winner.User) s"You win 🎉"
+    else "You lose 🤷"
   }
 
-  def checkWinner(user: Int, cpu: Int): Int = {
+  def checkWinner(user: String, cpu: String): String = {
     if (user == cpu) {
       Winner.Draw
     } else if (
-      (user == Weapon.Paper && cpu == Weapon.Scissor)
-      || (user == Weapon.Scissor && cpu == Weapon.Rock)
-      || (user == Weapon.Rock && cpu == Weapon.Paper)
+      (user == Weapon.Rock && cpu == Weapon.Scissor)
+      || (user == Weapon.Paper && cpu == Weapon.Rock)
+      || (user == Weapon.Scissor && cpu == Weapon.Paper)
     ) {
-      Winner.Cpu
-    } else {
       Winner.User
-    }
-  }
-
-  /** TODO should throw if input is not a valid weapon
-    *
-    * @param input
-    * @return
-    */
-  def parseWeapon(input: String): Int = {
-    if (input == s"${Weapon.Rock}") {
-      Weapon.Rock
-    } else if (input == s"${Weapon.Paper}") {
-      Weapon.Paper
-    } else if (input == s"${Weapon.Scissor}") {
-      Weapon.Scissor
     } else {
-      Weapon.Invalid // TODO throw?
+      // also covers the case for an invalid weapon (e.g. user inputs "4")
+      Winner.Cpu
     }
   }
 
