f5a385e4f0683f548e5086ddad3db6d654b6e704
giammyisjammy <49064602+giammyisjammy@users.noreply.github.com>
2022-10-05 16:41:45 +0200

    🗃️ Configure database migrations

---
+++ src/main/resources/db/V1__game_history.sql
@@ -0,0 +1,26 @@
+-- TODO intuisco a cosa possa servire ma nel tutorial non viene fatta menzione 
+-- della necessità di questo file :\
+-- la spiegazione dettagliata di una migrazione è forse out of scope per il training?
+
+CREATE TYPE move AS ENUM (
+  'Rock',
+  'Paper',
+  'Scissors'
+);
+
+CREATE TYPE match_result AS ENUM (
+  'Win',
+  'Lose',
+  'Draw'
+);
+
+CREATE CAST (varchar AS move) WITH INOUT AS ASSIGNMENT;
+CREATE CAST (varchar AS match_result) WITH INOUT AS ASSIGNMENT;
+
+CREATE TABLE history (
+  id uuid PRIMARY KEY,
+  computer_move move NOT NULL,
+  user_move move NOT NULL,
+  result match_result NOT NULL,
+  occurred_at timestamptz NOT NULL
+);
